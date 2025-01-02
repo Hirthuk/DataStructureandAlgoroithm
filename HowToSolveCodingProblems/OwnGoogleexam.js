a16d047af6a30a2cb3eea8b95858c22462d340b7
Hirthuk <sharankumarp02@gmail.com>
2025-01-02 22:15:17 +0530

    Day -3 Completed

--- HowToSolveCodingProblems/OwnGoogleexam.js
+++ HowToSolveCodingProblems/OwnGoogleexam.js
@@ -6,17 +6,26 @@
 // Worst case scenario using nested loops - which is easy for me but not suitable O(n^2) - time complexity
 // notation
 
+const exampleArray = [1,2,3,4,5,6,7,8];
+const sum = 11;
 function FindSumPair(array, sumNumber) {
   array.forEach((item) => {
     array.forEach((item2) => {
       let addValue = item + item2;
       if (sumNumber == addValue) {
         console.log(`Pair is there ${addValue}`);
-
-        return true; // it returns function will stop
       }
+      return true; // it returns function will stop
     });
   });
 }
 
+function Betterway(array, sum){
+ 
+
+}
+
 FindSumPair([1, 2], 3);
+
+//Different better way to do this -
+// https://www.youtube.com/watch?v=21pmwl0hrME - this one and this problem try to solve
