4a568256edd81383fe1db1146b81d97326557332
Hirthuk <sharankumarp02@gmail.com>
2025-01-05 22:09:52 +0530

    Day 6 Section 4 done

--- HowToSolveCodingProblems/CourseInterview.js
+++ HowToSolveCodingProblems/CourseInterview.js
@@ -66,3 +66,59 @@ usingInclude(array1, arrary2);
 // - Growth is significantly faster, making it less efficient for large inputs. 
 
 // If the loop is not nested but one after another mostly a+b
+
+// All code in Github repo
+// https://github.com/aneagoie/ztm-master-the-coding-interview-ds-algo
+
+// Angelo course implementation things instead of hash using object to store and retrive
+
+
+function containsCommonItem(arr1, arr2) {
+    for (let i=0; i < arr1.length; i++) {
+      for ( let j=0; j < arr2.length; j++) {
+        if(arr1[i] === arr2[j]) {
+          return true;
+        }
+      }
+    }
+    return false
+  }
+  
+  //O(a*b)
+  //O(1) - Space Complexity
+  
+//   const array1 = ['a', 'b', 'c', 'x'];
+//   const array2 = ['z', 'y', 'a'];
+  
+  function containsCommonItem2(arr1, arr2) {
+    // loop through first array and create object where properties === items in the array
+    // can we assume always 2 params?
+  
+    let map = {};
+    for (let i=0; i < arr1.length; i++) {
+      if(!map[arr1[i]]) {
+        const item = arr1[i];
+        map[item] = true;
+      }
+    }
+    // loop through second array and check if item in second array exists on created object. 
+    for (let j=0; j < arr2.length; j++) {
+      if (map[arr2[j]]) {
+        return true;
+      }
+    }
+    return false
+  }
+  
+  //O(a + b) Time Complexity
+  //O(a) Space Complexity
+  
+  // containsCommonItem2(array1, array2)
+//    For more readable and simple things we can show this later
+  function containsCommonItem3(arr1, arr2) {
+    return arr1.some(item => arr2.includes(item))
+  }
+  
+  containsCommonItem(array1, array2)
+  containsCommonItem2(array1, array2)
+  containsCommonItem3(array1, array2)
