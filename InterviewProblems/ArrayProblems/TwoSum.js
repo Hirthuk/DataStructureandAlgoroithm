d50b5d572c0634487e155d701b75bb774123ef95
Hirthuk <sharankumarp02@gmail.com>
2025-02-18 12:18:38 +0530

    My first leetcode problem

---
+++ InterviewProblems/ArrayProblems/TwoSum.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// Brute force approach
+const nums = [3,4,2];
+const target = 6;
+// Brute force approach
+// All test cases passed but it's O(n^2) Worst case
+var twoSum = function (nums, target) {
+    ReturnArray = [];
+  
+    if (nums.length == 0) {
+      return ReturnArray;
+    } else if (nums.length >= 2) {
+      for (let i = 0; i <= nums.length; i++) {
+        for (let j = i+1; j <= nums.length; j++) {
+          if (nums[i] + nums[j] == target) {
+            ReturnArray.push(i);
+            ReturnArray.push(j);
+            console.log(ReturnArray);
+            return ReturnArray;
+          }
+        }
+      }
+    }
+  };
+
+twoSum(nums, target);
+
+// Working on another worst case using includes 
+// Using hashmap would be the best option since finding number is O(1)
+// Or any other innovative which can lessen the things
+var remain = 0;
+var twoSum1 = function (nums, target) {
+
+    const ReturnArray = [];
+    for (let i = 0; i <= nums.length; i++) {
+        remain = target - nums[0]
+        if (nums.includes(remain)) {
+            ReturnArray.push(i);
+            const numRemain = nums.indexOf(remain);
+            ReturnArray.push(numRemain);
+            return ReturnArray;
+        }
+    }
+};
