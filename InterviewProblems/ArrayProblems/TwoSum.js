b54ce1c97cb239879d53ee86e66a4ef4137040cf
Hirthuk <sharankumarp02@gmail.com>
2025-02-19 10:50:03 +0530

    Day 3 of 180 Two Sum array completed

--- InterviewProblems/ArrayProblems/TwoSum.js
+++ InterviewProblems/ArrayProblems/TwoSum.js
@@ -67,3 +67,53 @@ var twoSum = function (nums, target) {
       
   }
 };
+
+// Using HashMap - seen and did
+// First storing all data to hashmap and making use of hashmap's has and get which is O(1)
+var twoSum = function (nums, target) {
+  const Hashmap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+      Hashmap.set(nums[i], i);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+      const compliment = target - nums[i];
+      if (Hashmap.has(compliment) && Hashmap.get(compliment) != i) {
+          return [i, Hashmap.get(compliment)]
+      }
+  }
+  return [];
+};
+
+// Better and final version
+// Here in if we are not check Hashmap.get(compliment) != i condition because the i element is not getting
+// Added if not there only it will add so no need. above we already added every key and values from array
+var twoSum = function(nums, target) {
+  const HashMap = new Map();
+  for(let i=0; i < nums.length; i++){
+      const compliment = target - nums[i];
+      if(HashMap.has(compliment)){
+          return [i, HashMap.get(compliment)]
+      }
+      HashMap.set(nums[i],i);
+  }
+  return []; //If Don't have choice
+};
+
+// Best Version with 0ms how it worked let's c
+var twoSum = function (nums, target) {
+  var object = {};
+
+  for (let i = 0; i < nums.length; i++) {
+      var compliment = target - nums[i];
+      if (compliment in object) {
+          return [object[compliment], i]
+      }
+      object[nums[i]] = i;
+
+  }
+};
+
+// Object can be added and retrived like this part from . operation 
+// Best use case in dynamic operation like getting from variable
+// Here instead of hashmap object is used that's only difference
---
+++ InterviewProblems/ArrayProblems/check.js
@@ -0,0 +1,11 @@
+// Just to check the things hows work
+
+const object = {
+    MyKey: "Sharan",
+}
+
+// object["sharan"] = "Anothername";
+
+
+console.log(object["MyKey"]);
+console.log(object.MyKey);
