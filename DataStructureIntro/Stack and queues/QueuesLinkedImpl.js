82aa9315965984873686e894421e2b13500eb5ba
Hirthuk <sharankumarp02@gmail.com>
2025-03-21 22:55:15 +0530

    Trees started

--- DataStructureIntro/Stack and queues/QueuesLinkedImpl.js
+++ DataStructureIntro/Stack and queues/QueuesLinkedImpl.js
@@ -19,18 +19,31 @@ class Queue {
   }
   enqueue(value) {
     let newNode = new node(value);
-    if(!this.top){
-        this.top = newNode;
-        this.end = newNode;
+    if (!this.top) {
+      this.top = newNode;
+      this.end = newNode;
+      this.length++
+      return;
     }
-    // Yet to complete enqueue
+    let temp = this.end.next;
+    this.end.next = newNode;
+    this.end = temp;
+    this.length++;
+    return ;
   }
-  dequeue(){
-    if(!this.top){
-        return null;
+  dequeue() {
+    if (!this.top) {
+      return null;
     }
     let temp = this.top.next;
     this.top = temp;
-    this.length--
+    this.length--;
+    return;
   }
 }
+
+const myQueue = new Queue();
+myQueue.enqueue(5);
+myQueue.enqueue(10);
+myQueue.dequeue();
+console.log(myQueue);
